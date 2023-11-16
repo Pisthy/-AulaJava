622c58242fa40b51282b9e78be2f80d54b49d935
Marcus Vinicius <marcusbol28@gmail.com>
2023-11-16 18:27:04 -0300

    atividade

---
+++ apache-maven-3.9.5/atividade8/src/test/java/br/ucb/poo/PessoaTest.java
@@ -0,0 +1,25 @@
+package br.ucb.poo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PessoaTest{
+
+    Pessoa pessoa;
+
+    @BeforeEach
+    void setUp(){
+        this.pessoa = new Pessoa();
+    }
+
+    @Test
+    void dizerOlaTest() {
+        assertEquals(pessoa.dizerOla(), "Olá, maven!");
+        System.out.println("OLÁ MARVEM");
+    }
+    
+
+    
+
+}
